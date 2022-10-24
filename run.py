ea9d7d52078d19f9023639831f4b5c7f5f1a070b
trevthedev777 <bloodsportrecords@gmail.com>
2022-10-24 02:52:12 +0000

    create routes.py file and plu the base.html, open on local port to see the app is running

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from taskmanager import app
+
+# to run the app we need to specify how and why
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
---
+++ taskmanager/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env
+
+
+# create an instance of the Flask class
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLACHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
