b913da08b28df1e6d22310caf1105b0ab1024045
trevthedev777 <bloodsportrecords@gmail.com>
2022-10-30 21:57:42 +0000

    migrate database

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,13 +2,13 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
 
 
 # create an instance of the Flask class
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLACHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,7 +2,7 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    # schema for the Category Model
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)  # noqa
@@ -13,7 +13,7 @@ class Category(db.Model):
 
 
 class Task(db.Model):
-    # scchema foor the Task model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
