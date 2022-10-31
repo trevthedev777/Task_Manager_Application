079f8a8b066b2af73fc58eb62abb0e481463993d
trevthedev777 <bloodsportrecords@gmail.com>
2022-10-31 10:23:07 +0000

    create function to delete a recoord off the db

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -39,3 +39,12 @@ def edit_category(category_id):
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+
+# delete the query
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
