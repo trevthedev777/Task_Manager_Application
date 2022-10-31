2d4b19c5e2dac2974107057d019779198c69cd22
trevthedev777 <bloodsportrecords@gmail.com>
2022-10-31 12:58:18 +0000

    add ask tasks form and layout

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,16 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+// datepicker init
+$(document).ready(function(){
+  $('.datepicker').datepicker(
+    format, "mmmm dd, yyyy", 
+    i18n, {done: "Select"}
+    );
+});
+
+// select init 
+$(document).ready(function(){
+  $('select').formSelect();
+});
